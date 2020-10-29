c195bbdff2cf716a9ae9714672bc1ed478bfaf11
Squappi <69323875+Squappi@users.noreply.github.com>
2020-10-29 11:40:43 +0300

    final Exam 8

--- js/script.js
+++ js/script.js
@@ -6,6 +6,49 @@ const iconName = modalShow.querySelector(".form-icon-name");
 const iconEmail = modalShow.querySelector(".form-icon-email");
 const textArea = modalShow.querySelector(".textarea-modal");
 const button = modalShow.querySelector(".button-modal");
+const slider = document.querySelector(".banner-style");
+const slides = document.getElementsByClassName("slide");
+const sliderControls = document.querySelector(".slider-controls");
+const dods = (sliderControls)?sliderControls.children:null;
+
+let current = 0;
+let prevent = 2;
+
+let timerId;
+getTimeout();
+function getTimeout(){
+	clearTimeout(timerId);
+	timerId = setTimeout(function tick() {
+       	if(dods){
+       		current = (current + 1) % 3;
+			prevent = (current === 0)?2:current - 1;
+       		nextSlide();
+       		timerId = setTimeout(tick, 4000);
+       	}
+   	}, 4000);
+}
+          
+function nextSlide(){
+	slider.classList.remove("site-wrapper-" + prevent);
+	slider.classList.add("site-wrapper-" + current);
+	slides[prevent].classList.remove("slide-current");
+	slides[current].classList.add("slide-current");
+	dods[prevent].classList.remove("current");
+	dods[current].classList.add("current");
+}
+if(dods){
+	for (let i=0; i < dods.length; i++){
+		dods[i].addEventListener("click",function(){
+			let id = Number(dods[i].id);
+			if(id!== current){
+				prevent = current;
+				current = id;
+				nextSlide();
+				getTimeout();
+			}
+		});
+	}
+}
 
 
 buttonCart.addEventListener("click", function(evt) {
